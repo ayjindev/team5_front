d5b666ec90eee73844441bbc127a00e20f74204d
jay0v0 <youreawsome.ovob@gmail.com>
2022-02-23 14:24:55 +0900

    signup.vue edit-3

--- src/views/auth/signup.vue
+++ src/views/auth/signup.vue
@@ -2,13 +2,14 @@
   <div style="padding-top: 5%">
     <div>
       <b-row align-h="center">
-        <b-col cols="5">
+        <b-col cols="3">
           <b-card title="회원가입">
             <b-form-group>
               <label for="userId">아이디</label>
               <b-form-input
                 id="userId"
                 v-model="userId"
+                v-bind="validateId"
                 :state="userIdState"
                 aria-describedby="아이디"
                 required
@@ -18,7 +19,7 @@
 
               <!-- 조건 미충족 시 -->
               <b-form-invalid-feedback id="input-live-feedback">
-                아이디는 5-12글자 사이여야 합니다.
+                아이디는 영문 대소문자와 숫자 5-12자리로 입력해야 합니다
               </b-form-invalid-feedback>
 
               <!-- 조건 충족 시 -->
@@ -40,7 +41,7 @@
 
               <!-- 조건 미충족 시 -->
               <b-form-invalid-feedback id="input-live-feedback">
-                비밀번호는 8-20글자 사이여야 합니다.
+                비밀번호는 영문 대소문자, 숫자, 특수문자 8-20자리로 입력해야 합니다
               </b-form-invalid-feedback>
 
               <!-- 조건 충족 시 -->
@@ -83,7 +84,7 @@
 
               <!-- 조건 미충족 시 -->
               <b-form-invalid-feedback id="input-live-feedback">
-                최소 2글자 이상 입력해 주세요
+                이름은 한글 2자리 이상 입력해야 합니다
               </b-form-invalid-feedback>
 
               <!-- 조건 충족 시 -->
@@ -100,8 +101,16 @@
                 placeholder="example@example.com"
                 size="sm"
                 required
+                :state="userEmailState"
                 trim
+                @blur="checkDuplicate"
               ></b-form-input>
+
+              <!-- 조건 미충족 시 -->
+              <b-form-invalid-feedback id="input-live-feedback"> 이메일 형식이 다릅니다 </b-form-invalid-feedback>
+
+              <!-- 조건 충족 시 -->
+              <b-form-text id="input-live-help"></b-form-text>
             </b-form-group>
 
             <b-form-group>
@@ -110,12 +119,13 @@
                 id="userPhoneNumber"
                 v-model="userPhoneNumber"
                 aria-describedby="전화번호"
-                placeholder="000-0000-0000"
+                placeholder="숫자만 입력해 주세요"
                 type="text"
                 size="sm"
                 :formatter="formatNumber"
                 required
                 trim
+                @keyup="getPhoneMask(userPhoneNumber)"
               ></b-form-input>
             </b-form-group>
 
@@ -133,16 +143,28 @@
             </b-form-group>
 
             <b-form-group class="daummap">
-              <b-row>
+              <b-row align-h="center">
                 <label for="userAddress">주소</label>
-                <b-col>
-                  <b-form-input id="userZip" v-model="zip" type="text" placeholder="우편번호" size="sm"></b-form-input>
+                <b-col cols="5">
+                  <b-form-input
+                    id="userZip"
+                    v-model="userZip"
+                    type="text"
+                    placeholder="우편번호"
+                    size="sm"
+                  ></b-form-input>
                 </b-col>
                 <b-col> <b-button size="sm" @click="showApi">우편번호 찾기</b-button></b-col>
-                <b-form-input id="userAddr1" v-model="addr1" type="text" placeholder="주소" size="sm"></b-form-input>
+                <b-form-input
+                  id="userAddr1"
+                  v-model="userAddr1"
+                  type="text"
+                  placeholder="주소"
+                  size="sm"
+                ></b-form-input>
                 <b-form-input
                   id="userAddr2"
-                  v-model="addr2"
+                  v-model="userAddr2"
                   type="text"
                   placeholder="상세주소"
                   size="sm"
@@ -152,6 +174,7 @@
           </b-card>
         </b-col>
       </b-row>
+      <button type="submit" class="btn btn-primary">Sign in</button>
     </div>
   </div>
 </template>
@@ -168,29 +191,88 @@ export default {
       userEmail: '',
       userPhoneNumber: '',
       userBirth: '',
-      zip: '',
-      addr1: '',
-      addr2: ''
+      userZip: '',
+      userAddr1: '',
+      userAddr2: ''
     }
   },
   computed: {
     userIdState() {
-      return this.userId.length > 4 && this.userId.length < 13
+      return this.userId.length > 4 && this.userId.length < 13 && /^[a-zA-Z0-9]*$/.test(this.userId)
     },
     userPwState() {
-      return this.userPw.length > 7 && this.userPw.length < 21
+      return (
+        this.userPw.length > 7 &&
+        this.userPw.length < 21 &&
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]*$/.test(this.userPw)
+      ) // 영문 대문자+소문자+특수문자 8-20자리
     },
     userPwCheckState() {
       return this.userPwCheck === this.userPw
     },
     userNameState() {
-      return this.userName.length > 1 ? true : false
+      return this.userName.length > 1 && /^[가-힣]*$/.test(this.userName) // 한글 2자리 이상
+    },
+    userEmailState() {
+      return (
+        this.userEmail.length > 5 &&
+        /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/.test(this.userEmail)
+      ) // 이메일 형식(영문대소문자/숫자+@+영문대소문자/숫자+.+영문대소문자 3자리)
     }
   },
   methods: {
+    // 전화번호 글자수 제한
     formatNumber(e) {
-      return String(e).substring(0, 13)
+      return String(e).substring(0, 13) // 최대 11자리 010-1234-5678
     },
+
+    // 전화번호 숫자만 입력 시 파이프(-) 자동 입력
+    getPhoneMask(val) {
+      let res = this.getMask(val)
+      this.userPhoneNumber = res
+
+      // 서버 전송 값에는 '-'를 제외하고 숫자만 저장
+      this.model.userPhoneNumber = this.userPhoneNumber.replace(/[^0-9]/g, '')
+    },
+
+    getMask(inputNumber) {
+      if (!inputNumber) return inputNumber
+      inputNumber = inputNumber.replace(/[^0-9]/g, '')
+
+      let res = ''
+      if (inputNumber.length < 3) {
+        res = inputNumber
+      } else {
+        if (inputNumber.substr(0, 2) == '02') {
+          if (inputNumber.length <= 5) {
+            //02-123-4567
+            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3)
+          } else if (inputNumber.length > 5 && inputNumber.length <= 9) {
+            //02-123-4567
+            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3) + '-' + inputNumber.substr(5)
+          } else if (inputNumber.length > 9) {
+            //02-1234-5678
+            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 4) + '-' + inputNumber.substr(6)
+          }
+        } else {
+          if (inputNumber.length < 8) {
+            res = inputNumber
+          } else if (inputNumber.length == 8) {
+            res = inputNumber.substr(0, 4) + '-' + inputNumber.substr(4)
+          } else if (inputNumber.length == 9) {
+            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
+          } else if (inputNumber.length == 10) {
+            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
+          } else if (inputNumber.length > 10) {
+            // 010-1234-5678
+            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 4) + '-' + inputNumber.substr(7)
+          }
+        }
+      }
+      return res
+    },
+
+    // 우편번호 찾기 다음 우편번호 서비스 api 연동
     showApi() {
       new window.daum.Postcode({
         oncomplete: data => {
@@ -220,8 +302,8 @@ export default {
           }
 
           // 우편번호와 주소 정보를 해당 필드에 넣는다.
-          this.zip = data.zonecode // 5자리 새 우편번호 사용
-          this.addr1 = fullRoadAddr
+          this.userZip = data.zonecode // 5자리 새 우편번호 사용
+          this.userAddr1 = fullRoadAddr
         }
       }).open()
     }
