10844d78c2ce6935848f8004758858727014e6bf
jay0v0 <youreawsome.ovob@gmail.com>
2022-02-24 19:27:35 +0900

    회원가입 폼 post 연동

--- src/views/auth/signup.vue
+++ src/views/auth/signup.vue
@@ -8,7 +8,7 @@
               <label for="userId">아이디</label>
               <b-form-input
                 id="userId"
-                v-model="userId"
+                v-model="user.userId"
                 :state="userIdState"
                 aria-describedby="아이디"
                 required
@@ -29,7 +29,7 @@
               <label for="userPw">비밀번호</label>
               <b-form-input
                 id="userPw"
-                v-model="userPw"
+                v-model="user.userPw"
                 :state="userPwState"
                 aria-describedby="비밀번호"
                 type="password"
@@ -51,7 +51,7 @@
               <label for="userPw">비밀번호 재확인</label>
               <b-form-input
                 id="userPwCheck"
-                v-model="userPwCheck"
+                v-model="user.userPwCheck"
                 :state="userPwCheckState"
                 aria-describedby="비밀번호 재확인"
                 type="password"
@@ -73,7 +73,7 @@
               <label for="userName">이름</label>
               <b-form-input
                 id="userName"
-                v-model="userName"
+                v-model="user.userName"
                 :state="userNameState"
                 aria-describedby="이름"
                 size="sm"
@@ -94,7 +94,7 @@
               <label for="userEmail">이메일</label>
               <b-form-input
                 id="userEmail"
-                v-model="userEmail"
+                v-model="user.userEmail"
                 aria-describedby="이메일"
                 type="email"
                 placeholder="example@example.com"
@@ -112,18 +112,18 @@
             </b-form-group>
 
             <b-form-group>
-              <label for="userPhoneNumber">전화번호</label>
+              <label for="userPhoneNumber">핸드폰 번호</label>
               <b-form-input
                 id="userPhoneNumber"
-                v-model="userPhoneNumber"
-                aria-describedby="전화번호"
-                placeholder="숫자만 입력해 주세요"
+                v-model="user.userPhoneNumber"
+                aria-describedby="핸드폰 번호"
+                placeholder="000-0000-0000"
                 type="text"
                 size="sm"
                 :formatter="formatNumber"
                 required
                 trim
-                @keyup="getPhoneMask(userPhoneNumber)"
+                @keyup="getPhoneMask(user.userPhoneNumber)"
               ></b-form-input>
             </b-form-group>
 
@@ -131,7 +131,7 @@
               <label for="userEmail">생년월일</label>
               <b-form-input
                 id="userBirth"
-                v-model="userBirth"
+                v-model="user.userBirth"
                 aria-describedby="생년월일"
                 type="date"
                 size="sm"
@@ -146,7 +146,7 @@
                 <b-col cols="5">
                   <b-form-input
                     id="userZip"
-                    v-model="userZip"
+                    v-model="user.userZip"
                     type="text"
                     placeholder="우편번호"
                     size="sm"
@@ -155,14 +155,14 @@
                 <b-col> <b-button size="sm" @click="showApi">우편번호 찾기</b-button></b-col>
                 <b-form-input
                   id="userAddr1"
-                  v-model="userAddr1"
+                  v-model="user.userAddr1"
                   type="text"
                   placeholder="주소"
                   size="sm"
                 ></b-form-input>
                 <b-form-input
                   id="userAddr2"
-                  v-model="userAddr2"
+                  v-model="user.userAddr2"
                   type="text"
                   placeholder="상세주소"
                   size="sm"
@@ -179,45 +179,82 @@
 
 <script>
 export default {
-  name: 'Daummap',
+  // name: 'Daummap',
   data() {
     return {
-      userId: '',
-      userPw: '',
-      userPwCheck: '',
-      userName: '',
-      userEmail: '',
-      userPhoneNumber: '',
-      userBirth: '',
-      userZip: '',
-      userAddr1: '',
-      userAddr2: ''
+      user: {
+        userId: '',
+        userPw: '',
+        userPwCheck: '',
+        userName: '',
+        userEmail: '',
+        userPhoneNumber: '',
+        userBirth: '',
+        userZip: '',
+        userAddr1: '',
+        userAddr2: ''
+      }
     }
   },
   computed: {
+    infoData() {
+      return this.$store.getters.User
+    },
     userIdState() {
-      return this.userId.length > 4 && this.userId.length < 13 && /^[a-zA-Z0-9]*$/.test(this.userId)
+      return this.user.userId.length > 4 && this.user.userId.length < 13 && /^[a-zA-Z0-9]*$/.test(this.user.userId)
     },
     userPwState() {
       return (
-        this.userPw.length > 7 &&
-        this.userPw.length < 21 &&
-        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]*$/.test(this.userPw)
+        this.user.userPw.length > 7 &&
+        this.user.userPw.length < 21 &&
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]*$/.test(this.user.userPw)
       ) // 영문 대문자+소문자+특수문자 8-20자리
     },
     userPwCheckState() {
-      return this.userPwCheck === this.userPw
+      return this.user.userPwCheck === this.user.userPw
     },
     userNameState() {
-      return this.userName.length > 1 && /^[가-힣]*$/.test(this.userName) // 한글 2자리 이상
+      return this.user.userName.length > 1 && /^[가-힣]*$/.test(this.user.userName) // 한글 2자리 이상
     },
     userEmailState() {
       return (
-        this.userEmail.length > 5 &&
-        /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/.test(this.userEmail)
+        this.user.userEmail.length > 5 &&
+        /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/.test(this.user.userEmail)
       ) // 이메일 형식(영문대소문자/숫자+@+영문대소문자/숫자+.+영문대소문자 3자리)
+    },
+    insertedResult() {
+      return this.$store.getters.UserInsertedResult
     }
   },
+  watch: {
+    infoData(value) {
+      console.log('watch.infoData', value)
+    },
+    insertedResult(value) {
+      console.log('watch.insertedResult', value)
+      // 등록 후 처리
+
+      if (value !== null) {
+        if (value > 0) {
+          // 등록이 성공한 경우
+
+          // 회원가입 완료 시 메인 페이지로 이동
+          this.$router.replace('/main')
+        } else {
+          // 회원가입 실패한 경우 토스트 메세지 출력
+          this.$bvToast.toast('등록이 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+          // this.$router.go() // 현재 페이지 새로고침
+        }
+      }
+    }
+  },
+  // created() {
+  //   this.user = { ...this.infoData }
+  // },
   methods: {
     // 전화번호 글자수 제한
     formatNumber(e) {
@@ -227,10 +264,10 @@ export default {
     // 전화번호 숫자만 입력 시 파이프(-) 자동 입력
     getPhoneMask(val) {
       let res = this.getMask(val)
-      this.userPhoneNumber = res
+      this.user.userPhoneNumber = res
 
-      // 서버 전송 값에는 '-'를 제외하고 숫자만 저장
-      this.model.userPhoneNumber = this.userPhoneNumber.replace(/[^0-9]/g, '')
+      // // 서버 전송 값에는 '-'를 제외하고 숫자만 저장
+      // this.$store.getters.User.userPhoneNumber = this.user.userPhoneNumber.replace(/[^0-9]/g, '')
     },
 
     getMask(inputNumber) {
@@ -238,35 +275,35 @@ export default {
       inputNumber = inputNumber.replace(/[^0-9]/g, '')
 
       let res = ''
-      if (inputNumber.length < 3) {
+      // if (inputNumber.length < 3) {
+      //   res = inputNumber
+      // } else {
+      //   if (inputNumber.substr(0, 2) == '02') {
+      //     if (inputNumber.length <= 5) {
+      //       //02-123까지만 입력 되어도 - 삽입
+      //       res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3)
+      //     } else if (inputNumber.length > 5 && inputNumber.length <= 9) {
+      //       //02-123-4567
+      //       res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3) + '-' + inputNumber.substr(5)
+      //     } else if (inputNumber.length > 9) {
+      //       //02-1234-5678
+      //       res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 4) + '-' + inputNumber.substr(6)
+      //     }
+      //   } else {
+      // 010-1234-5678
+      if (inputNumber.length < 8) {
         res = inputNumber
-      } else {
-        if (inputNumber.substr(0, 2) == '02') {
-          if (inputNumber.length <= 5) {
-            //02-123까지만 입력 되어도 - 삽입
-            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3)
-          } else if (inputNumber.length > 5 && inputNumber.length <= 9) {
-            //02-123-4567
-            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 3) + '-' + inputNumber.substr(5)
-          } else if (inputNumber.length > 9) {
-            //02-1234-5678
-            res = inputNumber.substr(0, 2) + '-' + inputNumber.substr(2, 4) + '-' + inputNumber.substr(6)
-          }
-        } else {
-          // 010-1234-5678
-          if (inputNumber.length < 8) {
-            res = inputNumber
-          } else if (inputNumber.length == 8) {
-            res = inputNumber.substr(0, 4) + '-' + inputNumber.substr(4)
-          } else if (inputNumber.length == 9) {
-            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
-          } else if (inputNumber.length == 10) {
-            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
-          } else if (inputNumber.length > 10) {
-            res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 4) + '-' + inputNumber.substr(7)
-          }
-        }
+      } else if (inputNumber.length == 8) {
+        res = inputNumber.substr(0, 4) + '-' + inputNumber.substr(4)
+      } else if (inputNumber.length == 9) {
+        res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
+      } else if (inputNumber.length == 10) {
+        res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 3) + '-' + inputNumber.substr(6)
+      } else if (inputNumber.length > 10) {
+        res = inputNumber.substr(0, 3) + '-' + inputNumber.substr(3, 4) + '-' + inputNumber.substr(7)
       }
+      //   }
+      // }
       return res
     },
 
@@ -300,27 +337,18 @@ export default {
           }
 
           // 우편번호와 주소 정보를 해당 필드에 넣는다.
-          this.userZip = data.zonecode // 5자리 새 우편번호 사용
-          this.userAddr1 = fullRoadAddr
+          this.user.userZip = data.zonecode // 5자리 새 우편번호 사용
+          this.user.userAddr1 = fullRoadAddr
         }
       }).open()
     },
     // 콘솔 로그 확인
     onSubmit() {
-      console.log(
-        'onSubmit',
-        this.userId,
-        this.userPw,
-        this.userPwCheck,
-        this.userPwCheck,
-        this.userName,
-        this.userEmail,
-        this.userPhoneNumber,
-        this.userBirth,
-        this.userZip,
-        this.userAddr1,
-        this.userAddr2
-      )
+      // console.log('onSubmit', { ...this.user })
+      // 초기화
+      this.$store.dispatch('actUserInit') // null값으로 초기화
+      // 등록
+      this.$store.dispatch('actUserInsert', this.user)
     }
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const { VUE_APP_SERVER } = process.env
+
 module.exports = {
+  // 개발 서버 설정
+  devServer: {
+    // 프로시 설정
+    proxy: {
+      // 프록시 요청을 보낼 api
+      '/serverApi': {
+        // 프록시 요청을 보낼 서버 주소
+        target: VUE_APP_SERVER, // .env 파일 참고
+        changeOrigin: true,
+        pathRewrite: {
+          '^/serverApi': ''
+        }
+      }
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
